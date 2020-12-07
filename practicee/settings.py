73817cc3a72935a2dc7e20353242cd868e0b1a30
khel <mwin31565@gmail.com>
2020-12-07 15:51:25 +0800

    added database conn

--- practicee/settings.py
+++ practicee/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     }
 }
 
+
+
 """
 DATABASES = {
     'default': {
@@ -91,6 +93,11 @@ DATABASES = {
     }
 }
 """
+
+import dj_database_url
+
+db_conn = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_conn)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
